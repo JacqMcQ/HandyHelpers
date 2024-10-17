4eb16df79851b785690783c802adb3b0d3f9e1de
ebaby-ak <166087675+ebaby-ak@users.noreply.github.com>
2024-10-16 21:13:22 -0500

    updated Login.jsx to handle user login using a mutation and state form input

---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,71 @@
+import { useState } from 'react';
+import { useMutation } from '@apollo/client';
+import { Link } from 'react-router-dom';
+import { LOGIN } from '../utils/mutations';
+import Auth from '../utils/auth';
+
+function Login(props) {
+    const [formState, setFormState] = useState({ email: '', password: '' });
+    const [login, { error }] = useMutation(LOGIN);
+
+    const handleFormSubmit = async (event) -> {
+        event.preventDefault();
+        try {
+            const mutationResponse = await login({
+                variables: { email: formState.email, password: formState.password },
+            });
+            
+            const token = mutationResponse.data.login.token;
+            Auth.login(token);
+        } catch (e) {
+            console.log(e);
+        }
+    };
+
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+        setFormState({
+            ...formState,
+            [name]: value,
+        });
+    };
+
+    return (
+        <div className='container my-1'>
+            <Link to='/signup'>Signup</Link>
+
+            <h2>Login</h2>
+            <form onSubmit={handleFormSubmit}>
+                <div className='flex-row space-between my-2'>
+                    <label>Email:</label>
+                    <input
+                        type='email'
+                        name='email'
+                        value={formState.email}
+                        onChange={handleChange}
+                    />
+                </div>
+            <div className='flex-row space-between my-2'>
+                <label>Password:</label>
+                <input
+                    type='password'
+                    name='password'
+                    type='password'
+                    id='pwd'
+                    onChange={handleChange}
+                />
+            </div>
+            {error ? (
+                <div>
+                    <p className='error-text'>Email or Password incorrect</p>
+                </div>
+            ) : null}
+            <div className='flex-row flex-end'>
+                <button type='submit'>Submit</button>
+            </div>
+            </form>
+        </div>
+    );
+}
+
+export default Login;
