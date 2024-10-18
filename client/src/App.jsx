25e2ca5be93c085ead7331e30d8595c1b28dddd9
ebaby-ak <166087675+ebaby-ak@users.noreply.github.com>
2024-10-17 20:47:24 -0500

    add mutations for login and signup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Login from './pages/Login';
 import CleaningServices from "./pages/CleaningServices";
 import Signup from "./pages/Signup"; // Import the Signup component
 
@@ -6,6 +7,7 @@ function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/login" element={<Login />} />
         <Route path="/cleaning-services" element={<CleaningServices />} />
         <Route path="/signup" element={<Signup />} /> 
       </Routes>
