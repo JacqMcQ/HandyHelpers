feefdfc52a19dddd8ca29047407fe51192f7cac0
chris <cvera8873@gmail.com>
2024-10-17 21:11:18 -0500

    created JWT files and updated server.js

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,9 +1,40 @@
-const jwt = require("jsonwebtoken");
+import jwt from "jsonwebtoken";
 
-const generateToken = (user) => {
-  return jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
-    expiresIn: "1h",
-  });
-};
 
-module.exports = { generateToken };
+// set token secret and expiration date
+const secret = process.env.JWT_SECRET;
+const expiration = '2h';
+
+
+  // function for our authenticated routes
+  export const authMiddleware = (req, res, next) => {
+    // allows token to be sent via  req.query or headers
+    let token = req.query.token || req.headers.authorization;
+
+    // ["Bearer", "<tokenvalue>"]
+    if (req.headers.authorization) {
+      token = token.split(' ').pop().trim();
+    }
+
+    if (!token) {
+      return res.status(400).json({ message: 'You have no token!' });
+    }
+
+    // verify token and get user data out of it
+    try {
+      const { data } = jwt.verify(token, secret, { maxAge: expiration });
+      req.user = data;
+    } catch {
+      console.log('Invalid token');
+      return res.status(400).json({ message: 'invalid token!' });
+    }
+
+    // send to next endpoint
+    next();
+  };
+
+  export const signToken = ({ username, email, _id }) => {
+    const payload = { username, email, _id };
+
+    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+  };
