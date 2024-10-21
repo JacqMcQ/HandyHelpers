4aeed7f8ac25099b49e37ccae31f05a62cb49a86
JacMcQ <jacqlyn.mcquade@icloud.com>
2024-10-21 13:16:12 -0600

    please push for correcting last merge confile unresolved

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,11 +9,11 @@ import { Outlet } from "react-router-dom";
 
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Nav from "./components/Nav";
 
 const httpLink = createHttpLink({
   uri: "/graphql",
 });
-
 const authLink = setContext((_, { headers }) => {
   const token = localStorage.getItem("id_token");
   return {
@@ -23,16 +23,15 @@ const authLink = setContext((_, { headers }) => {
     },
   };
 });
-
 const client = new ApolloClient({
   link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
-
 function App() {
   return (
     <ApolloProvider client={client}>
       <div className="flex-column">
+        <Nav />
         <Header />
         <div className="container">
           <Outlet />
