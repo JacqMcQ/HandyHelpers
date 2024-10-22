ebdda5beaf7a31cff456eb3d2e803e679d355ca6
ebaby-ak <166087675+ebaby-ak@users.noreply.github.com>
2024-10-21 19:14:33 -0500

    added routes to nav.jsx, added styling and tagline to header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Auth from "../utils/auth";
 
 const Header = () => {
+  // if user is logged in, get username
+  const username = Auth.loggedIn() ? Auth.getProfile().data.username : null;
+
   return (
     <header>
       <div className="header-container">
@@ -15,6 +19,9 @@ const Header = () => {
           </figure>
         </Link>
         <h1 className="header-title">Handy Helpers</h1>
+        {/* Greet user if logged in */}
+        {username && <p className="header-greeting">Welcome, {username}!</p>}
+        <p className="header-tagline">Your go-to home solutions!</p>
       </div>
     </header>
   );
