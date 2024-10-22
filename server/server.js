c63f0843d38e262e793967ad98c83a1773c886f1
JacMcQ <jacqlyn.mcquade@icloud.com>
2024-10-21 20:06:44 -0600

    debug for server

--- server/server.js
+++ server/server.js
@@ -14,7 +14,6 @@ dotenv.config();
 
 const app = express(); // You need to define the `app` variable
 
-
 // Get __dirname equivalent
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -31,7 +30,6 @@ app.use("/api/google-places", googlePlacesRoutes);
 // Route to retrieve addresses from the database
 app.use("/api/addresses", addressRoutes);
 
-
 const server = new ApolloServer({
   typeDefs,
   resolvers,
@@ -45,21 +43,24 @@ const startApolloServer = async () => {
   app.use(express.json());
 
   // Serve up static assets
-  app.use('/images', express.static(path.join(__dirname, '../client/images')));
+  app.use("/images", express.static(path.join(__dirname, "../client/images")));
 
-  app.use('/graphql', expressMiddleware(server, {
-    context: authMiddleware
-  }));
+  app.use(
+    "/graphql",
+    expressMiddleware(server, {
+      context: authMiddleware,
+    })
+  );
 
-  if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/dist')));
+  if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../client/dist")));
 
-    app.get('*', (req, res) => {
-      res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+    app.get("*", (req, res) => {
+      res.sendFile(path.join(__dirname, "../client/dist/index.html"));
     });
   }
 
-  db.once('open', () => {
+  db.once("open", () => {
     app.listen(PORT, () => {
       console.log(`API server running on port ${PORT}!`);
       console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
